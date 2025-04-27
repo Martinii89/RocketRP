9132fc96b81f22198821a549ddfcd179e4901763
Martin Normann <m4rtini89@gmail.com>
2025-04-27 21:47:16 +0200

    notify that we can't fix replays that does not have the property we try to rename

--- ReplayFixService/Program.cs
+++ ReplayFixService/Program.cs
@@ -73,8 +73,17 @@ app.MapPost("api/replayfix", async (IFormFile file, ReplayFixHandler handler) =>
     try
     {
         await using var fileStream = file.OpenReadStream();
-        var resultStream = await handler.Handle(fileStream);
-        return TypedResults.File(resultStream, "application/octet-stream", file.FileName);
+        var (stream, result) = await handler.Handle(fileStream);
+        if (result is ReplayFixHandler.RepackResult.MissingProperty)
+        {
+            return TypedResults.Problem("The replay does not seem to be broken", statusCode: 400);
+        }
+
+        if (stream is null)
+        {
+            return TypedResults.Problem("Internal error: Stream is null");
+        }
+        return TypedResults.File(stream, "application/octet-stream", file.FileName);
     }
     catch (Exception ex)
     {
@@ -89,7 +98,7 @@ app.Run();
 
 public class ReplayFixHandler(ILogger<ReplayFixHandler> logger)
 {
-    public Task<Stream> Handle(Stream fileStream)
+    public Task<(Stream? repackStream, RepackResult result)> Handle(Stream fileStream)
     {
         try
         {
@@ -100,17 +109,29 @@ public class ReplayFixHandler(ILogger<ReplayFixHandler> logger)
             {
                 replay.Objects[index] = "TAGame.PRI_TA:bPlayerHistoryValid";
             }
+            else
+            {
+                return Task.FromResult<(Stream?, RepackResult)>((null, RepackResult.MissingProperty));
+            }
             var stream = new MemoryStream();
             replay.Serialize(stream);
             stream.Position = 0;
             logger.LogInformation("Successfully processed replay");
-            return Task.FromResult<Stream>(stream);
+            return Task.FromResult<(Stream?, RepackResult)>((stream, RepackResult.Succes));
         }
         catch (Exception ex)
         {
             logger.LogError(ex, "Error processing replay");
             throw;
         }
+    }
+    
+    
+
+    public enum RepackResult
+    {
+        Succes, 
+        MissingProperty
     }
 }
 
